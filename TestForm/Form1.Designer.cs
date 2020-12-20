24b60de28f944750eedb93e41aa5ea383f3d939e
Justin Utesch <uteschj@gmail.com>
2020-12-20 12:47:36 -0600

    Labeling

--- TestForm/Form1.Designer.cs
+++ TestForm/Form1.Designer.cs
@@ -40,6 +40,8 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ThreadedTestButton
@@ -74,7 +76,7 @@
             // 
             // CreateFilesButton
             // 
-            this.CreateFilesButton.Location = new System.Drawing.Point(68, 12);
+            this.CreateFilesButton.Location = new System.Drawing.Point(71, 60);
             this.CreateFilesButton.Name = "CreateFilesButton";
             this.CreateFilesButton.Size = new System.Drawing.Size(145, 23);
             this.CreateFilesButton.TabIndex = 4;
@@ -112,7 +114,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(232, 17);
+            this.label4.Location = new System.Drawing.Point(235, 65);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(219, 13);
             this.label4.TabIndex = 8;
@@ -154,11 +156,33 @@
             this.label7.TabIndex = 12;
             this.label7.Text = "Approx Time: 1:26";
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(31, 57);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 24);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "1.";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(31, 138);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(25, 24);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "2.";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(638, 464);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -191,6 +215,8 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
